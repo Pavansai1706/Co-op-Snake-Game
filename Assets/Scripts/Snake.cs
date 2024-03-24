317ef325ea2bf8c0c24f6be9110b5635ce2fd0be
pavansai <thammupavan2002@gmail.com>
2024-03-24 20:08:44 +0530

    Death

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
@@ -5,8 +6,16 @@ public class Snake : MonoBehaviour
     private Vector2 _direction = Vector2.right;
     private Vector2 _screenBounds;
 
-    void Start()
+    private List<Transform> _segments = new List<Transform>();
+    [SerializeField] private int initialSize = 3;
+    [SerializeField] private Transform segmentPrefab;
+   
+
+
+    private void Start()
     {
+        ResetState();
+
         _screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
@@ -32,6 +41,12 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate()
     {
+
+        for(int i = _segments.Count -1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         // Calculate the new position based on the current position and direction
         Vector3 newPosition = transform.position + (Vector3)_direction;
 
@@ -48,5 +63,41 @@ public class Snake : MonoBehaviour
 
         // Update the position
         transform.position = newPosition;
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+    private void ResetState()
+    {
+        for (int i =1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        for (int i =1; i < this.initialSize; i++)
+        {
+            _segments.Add(Instantiate(this.segmentPrefab));
+        }
+
+        this.transform.position = Vector3.zero;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        }
+        else if (other.tag == "Obstacle"){
+            ResetState();
+        }
     }
 }
